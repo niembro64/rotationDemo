61c3e007412f2cc5d410535a841946bc6ced437c
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-03-15 10:09:52 -0400

    init

---
+++ rotationDemo.js
@@ -0,0 +1,14 @@
+const rotateString = (str,num) => {
+
+}
+
+
+const isRotation = (str1, str2) => {
+
+}
+
+a = "Did I shine my shoes today?"
+b = "es  today?Did  I shine my sho"
+c = "es  today ?Did  I shine my sho"
+
+console.log()
