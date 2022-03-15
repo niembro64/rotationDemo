a580ddbdaf9bdb1e8c7c43b83349b8011bd3d90a
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-03-15 10:15:05 -0400

    start

--- rotationDemo.js
+++ rotationDemo.js
@@ -1,14 +1,17 @@
-const rotateString = (str,num) => {
+const rotateString = (str, num) => {
+  newString = "";
 
-}
+  for (var i = 0; i < str.length; i++) {
+    newString.push(str[(i + 2) % str.length]);
+    console.log(`i: ${i} --- str.len: ${str.length}`);
+  }
+  return newString;
+};
 
+const isRotation = (str1, str2) => {};
 
-const isRotation = (str1, str2) => {
+a = "Did I shine my shoes today?";
+b = "es  today?Did  I shine my sho";
+c = "es  today ?Did  I shine my sho";
 
-}
-
-a = "Did I shine my shoes today?"
-b = "es  today?Did  I shine my sho"
-c = "es  today ?Did  I shine my sho"
-
-console.log()
+console.log();
